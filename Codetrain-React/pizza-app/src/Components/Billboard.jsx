745b84e23e4c6bcf5df4b0419e9d81c5981625d8
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2024-05-06 21:59:12 +0100

    created my new pizza file and uploaded the files I would need and created my components

---
+++ Codetrain-React/pizza-app/src/Components/Billboard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Billboard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Billboard

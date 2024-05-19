bc1509bd6ca8be8d7c4d0b6e4143a1964bcc6787
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2024-05-19 12:57:07 +0100

    deleted and changed folder name

---
+++ Home Practice/React-form-one/src/Components/Form.jsx
@@ -0,0 +1,13 @@
+import React, { useState} from 'react'
+
+function Form() {
+  return (
+    <div>
+      <form>
+        <h2>Member Login</h2>
+      </form>
+    </div>
+  )
+}
+
+export default Form

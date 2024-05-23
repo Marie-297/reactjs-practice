f45e40ee3490e292e9a12c007847504a188cfb49
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2024-05-23 21:55:28 +0100

    used map operator to loop through the list to display it on the User Interface

--- Codetrain-React/react-contacts/src/App.jsx
+++ Codetrain-React/react-contacts/src/App.jsx
@@ -3,12 +3,21 @@ import React, { useState } from 'react'
 function App() {
 
   const [contacts, setContacts] = useState([
-    {name: '', phoneNumber: '', location: ''}
+    {name: 'Phyllis Hackman', phoneNumber: '+233-544-366-122', location: 'American House'},
+    {name: 'Jason Mickmann', phoneNumber: '+233-550-144-002', location: 'Oyarifa Ghana-Flag'},
+    {name: 'Eunice Hagan', phoneNumber: '+233-234-888-452', location: 'Ogbojo Estate'},
+    {name: 'Phill Addison', phoneNumber: '+233-554-989-100', location: 'Cantonments'},
   ])
 
   return (
     <div>
-      
+      {contacts.map((contact, index) => (
+        <div>
+        <h3>{contact.name}</h3>
+        <p>{contact.phoneNumber}</p>
+        <p>{contact.location}</p>
+      </div>
+      ))}
     </div>
   )
 }
